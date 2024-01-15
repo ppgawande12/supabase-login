940c60e0a21e89126406b7bee8b134deb8943579
Pawan Gawande <129038360+ppgawande12@users.noreply.github.com>
2024-01-15 12:56:35 +0530

    SignUp.js

--- src/SignUp.js
+++ src/SignUp.js
@@ -25,7 +25,7 @@ const SignUp = () => {
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
           className="mx-auto h-10 w-auto"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+          src="https://magicainew.codeshastra.dev/assets/img/logo/magicAI-logo.svg"
           alt="Your Company"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
